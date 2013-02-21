55065635edefb2c04b618556b67f7d48e8338b79
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2013-02-20 21:06:06 -0500

    Added Service.php w/ sample array of assets encoded to JSON

---
+++ Service.php
@@ -0,0 +1,18 @@
+<?php
+
+$sample_properties = array(
+                         array(
+                            "Name" => "Google",
+                            "Image" => "http://git.juanleonardosanchez.com/AdClick-Prototype/web-assets/1.jpg", 
+                            "Url" => "http://adsyolo.com/openx/www/delivery/ck.php?n=1393b16"),
+                         array(
+                            "Name" => "AdsYolo",
+                            "Image" => "http://git.juanleonardosanchez.com/AdClick-Prototype/web-assets/2.jpg", 
+                            "Url" => "http://adsyolo.com/openx/www/delivery/ck.php?n=1ebfd84"),
+                         array(
+                            "Name" => "OpenX",
+                            "Image" => "http://git.juanleonardosanchez.com/AdClick-Prototype/web-assets/3.jpg", 
+                            "Url" => "http://adsyolo.com/openx/www/delivery/ck.php?n=4c13ebf")
+                   );
+
+echo json_encode($sample_properties);
