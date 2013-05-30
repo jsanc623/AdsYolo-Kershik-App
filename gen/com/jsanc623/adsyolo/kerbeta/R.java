da16c7abe03f1889edcf1a61e54dd5a3da14713a
juan.sanchez <juan.sanchez@juanleonardosanchez.com>
2013-05-30 15:49:39 -0400

    Major update + login system + xml parse + lazyadapter + memorycache + imageloader and filecache

--- gen/com/jsanc623/adsyolo/kerbeta/R.java
+++ gen/com/jsanc623/adsyolo/kerbeta/R.java
@@ -21,12 +21,29 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int arrow=0x7f020000;
+        public static final int dragracing=0x7f020001;
+        public static final int googlemusic=0x7f020002;
+        public static final int gradient_bg=0x7f020003;
+        public static final int gradient_bg_hover=0x7f020004;
+        public static final int heart=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int icerage=0x7f020007;
+        public static final int icon=0x7f020008;
+        public static final int image_bg=0x7f020009;
+        public static final int list_selector=0x7f02000a;
+        public static final int no_image=0x7f02000b;
+        public static final int play=0x7f02000c;
+        public static final int rihanna=0x7f02000d;
+        public static final int songza=0x7f02000e;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f080011;
+        public static final int artist=0x7f08000f;
         public static final int button1=0x7f080007;
-        public static final int listView1=0x7f080006;
+        public static final int duration=0x7f080010;
+        public static final int list=0x7f080006;
+        public static final int list_image=0x7f08000d;
         public static final int login_button=0x7f080005;
         public static final int password=0x7f080004;
         public static final int signupemail_input=0x7f080008;
@@ -36,6 +53,8 @@ public final class R {
         public static final int textView2=0x7f080000;
         public static final int textView3=0x7f080002;
         public static final int textView4=0x7f08000a;
+        public static final int thumbnail=0x7f08000c;
+        public static final int title=0x7f08000e;
         public static final int username=0x7f080003;
     }
     public static final class layout {
@@ -44,6 +63,7 @@ public final class R {
         public static final int activity_main=0x7f030002;
         public static final int activity_offers=0x7f030003;
         public static final int activity_signup=0x7f030004;
+        public static final int list_row=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/jsanc623/adsyolo/kerbeta/LoginActivity.java
+++ src/com/jsanc623/adsyolo/kerbeta/LoginActivity.java
@@ -1,40 +1,27 @@
 package com.jsanc623.adsyolo.kerbeta;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
-import com.appglu.AsyncCallback;
-import com.appglu.Row;
-import com.appglu.Rows;
-import com.appglu.android.AppGlu;
-import com.appglu.android.AppGluSettings;
-
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.appglu.android.AppGlu;
+import com.appglu.android.AppGluSettings;
+
+import android.util.Log;
 import android.widget.EditText;
-import android.app.Activity;
 import android.content.Intent;
 
 public class LoginActivity extends Activity {
@@ -43,192 +30,109 @@ public class LoginActivity extends Activity {
 	EditText passw_tmp;
 	String username;
 	String password;
+    TextView outputText;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		
-		Log.d("itsokall", "post setContentView");
-  
 	    Button loginButton = (Button) findViewById(R.id.login_button);
 	    loginButton.setOnClickListener(onClickListener);
 	    
-		Log.d("itsokall", "post .setOnClickListener");
-		
 		AppGluSettings settings = new AppGluSettings("fS3jywqBB3Iy27P", "LUbdT5GHTDmn14JYvUtzksFMttGJCU", "staging");
 		AppGlu.initialize(this, settings);
-		 
-		Log.d("itsokall", "post AppGlu init");
 	}
 	
 	public OnClickListener onClickListener = new OnClickListener() {
 	    public void onClick(final View v) {
-			 Log.d("itsokall", "in onClick pre switch");
+	        GetXMLTask task = new GetXMLTask();
              switch(v.getId()){
                  case R.id.login_button: {
-             		Log.d("itsokall", "in switch, getting username and password");
 					uname_tmp = (EditText)findViewById(R.id.username);
 					passw_tmp = (EditText)findViewById(R.id.password);
 					
 					username = uname_tmp.getText().toString();
 					password = passw_tmp.getText().toString();
 
-					Log.d("itsokall", username);
-					Log.d("itsokall", password);
-
-					Log.d("itsokall", "in switch,  building urlParameters");
+					String URL = "http://pixls.me/adsyolo/login.php?username=" + username + "&password=" + password;
 					
-					String urlParameters = null;
+					AsyncTask<String, String, String> userLoggedInResult = task.execute(new String[] { URL });
+					
+					String userLoggedIn = "";
 					try {
-						urlParameters = "username=" + URLEncoder.encode(username, "UTF-8") +
-						                "&password=" + URLEncoder.encode(password, "UTF-8");
-					} catch (UnsupportedEncodingException e) {
+						userLoggedIn = userLoggedInResult.get();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					} catch (ExecutionException e) {
 						e.printStackTrace();
 					}
-
-					/*Log.d("itsokall", "AppGlu CrudAPI"); 
 					
-					Rows result = AppGlu.crudApi().readAll("Offers");
-					Integer totalRows = result.getTotalRows();
-					List<Row> rows = result.getRows();
+					Log.d("itsokall", userLoggedIn.toString());
 					
-					// error here
-					AppGlu.crudApi().read("Offers", "1");
-					
-					AppGlu.crudApi().readInBackground("Offers", "1", new AsyncCallback<Row>() {
-						public void onResult(Row row) {
-						  Log.d("itsokall", "in onResult!");
-						  Log.d("itsokall", row.toString());
-						}
-					});*/
-					
-					Log.d("itsokall", "init Retriever Retriever = new Retriever. Then executePost.");
-					
-					Retriever Retriever = new Retriever();
-					String userLoggedIn = Retriever.executePost("http://pixls.me/adsyolo/login.php", username, password);
-					
-					Log.d("itsokall", "userLoggedIn" + userLoggedIn);
-					Log.d("itsokall", "entering userloggedin comparison");
-					if(userLoggedIn != null && !userLoggedIn.isEmpty()){
+					if(userLoggedIn != null && !userLoggedIn.toString().isEmpty()){
 						if(userLoggedIn.equals("fail")){
-							Log.d("itsokall", "in userloggedin comparaison, ok!");
 							Intent myIntent = new Intent(LoginActivity.this, SignupActivity.class);
 							LoginActivity.this.startActivity(myIntent);
 						} else {
-							Log.d("itsokall", "in userloggedin comparison, fail");
+							Log.d("itsokall", "in userloggedin comparison, ok");
 							Intent myIntent = new Intent(LoginActivity.this, OffersActivity.class);
 							LoginActivity.this.startActivity(myIntent);
 						}
-						Log.d("itsokall", "post everything in switch onCreate()");
-					} else {
-						Log.d("itsokall", "empty!"); 
 					}
                  }
              }
 	    }
 	};
-}
-
-
-class Retriever extends AsyncTask<String, String, String> {
 	
-	HttpResponse response;
-
-	public String executePost(String targetURL, String username, String password){
-
-		Log.d("itsokall", "in executePost " + targetURL);
-		
-		// Create a new HttpClient and Post Header
-		HttpClient httpclient = new DefaultHttpClient();
-		HttpPost httppost = new HttpPost(targetURL);
-
-		try {
-			Log.d("itsokall", "inTryBlock");
-		    // Add your data
-		    List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-		    nameValuePairs.add(new BasicNameValuePair("username", username));
-		    nameValuePairs.add(new BasicNameValuePair("password", password));
-		    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-		    
-		    Log.d("itsokall", "executing!");
-		    
-		    // Execute HTTP Post Request
-		    response = httpclient.execute(httppost);
-
-		} catch (ClientProtocolException e) {
-		    // TODO Auto-generated catch block
-		} catch (IOException e) {
-		    // TODO Auto-generated catch block
-		}
-
-		return response.toString();
-		
-	    /*URL url;
-	    HttpURLConnection connection = null;  
-	    try {
-	      Log.d("itsokall", "in executePost try-catch statement");
-	      
-	      //Create connection
-	      url = new URL(targetURL);
-	      connection = (HttpURLConnection)url.openConnection();
-	      connection.setRequestMethod("POST");
-	      connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-				
-	      connection.setRequestProperty("Content-Length", "" + 
-	               Integer.toString(urlParameters.getBytes().length));
-	      connection.setRequestProperty("Content-Language", "en-US");  
-				
-	      connection.setUseCaches(false);
-	      connection.setDoInput(true);
-	      connection.setDoOutput(true);
-	      
-
-	      Log.d("itsokall", "engaging connection");
-	      
-	      //Send request
-	      DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
-	      
-	      Log.d("itsokall", "connection engaged, writing to output stream");
-	      
-	      wr.writeBytes (urlParameters);
-	      wr.flush ();
-	      wr.close ();
-	      
-	      Log.d("itsokall", "dataoutputstream closed, getting response");
-
-	      //Get Response	
-	      InputStream is = connection.getInputStream();
-	      BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-	      String line;
-	      StringBuffer response = new StringBuffer(); 
-	      while((line = rd.readLine()) != null) {
-	        response.append(line);
-	        response.append('\r');
-	      }
-	      rd.close();
-	      Log.d("itsokall", "returning response: " + response.toString());
-
-			Log.d("itsokall", "Disengaging connection");
-			connection.disconnect(); 
-			
-	      return response.toString();
-	    } catch (Exception e) {
-	      e.printStackTrace();
-	      return null;
-	    }*/
-	  }
-
-    protected void onPostExecute() {
-    	Log.d("itsokall", "IN ONPOSTEXECUTE");
-        // TODO: check this.exception
-        // TODO: do something with the feed
-    }
-
-	@Override
-	protected String doInBackground(String... arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	private class GetXMLTask extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String output = null;
+            for (String url : urls) {
+                output = getOutputFromUrl(url);
+            }
+            return output;
+        }
+ 
+        private String getOutputFromUrl(String url) {
+            StringBuffer output = new StringBuffer("");
+            try {
+                InputStream stream = getHttpConnection(url);
+                BufferedReader buffer = new BufferedReader(
+                        new InputStreamReader(stream));
+                String s = "";
+                while ((s = buffer.readLine()) != null)
+                    output.append(s);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return output.toString();
+        }
+ 
+        // Makes HttpURLConnection and returns InputStream
+        private InputStream getHttpConnection(String urlString) throws IOException {
+            InputStream stream = null;
+            URL url = new URL(urlString);
+            URLConnection connection = url.openConnection();
+ 
+            try {
+                HttpURLConnection httpConnection = (HttpURLConnection) connection;
+                httpConnection.setRequestMethod("GET");
+                httpConnection.connect();
+ 
+                if (httpConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    stream = httpConnection.getInputStream();
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return stream;
+        }
+ 
+        @Override
+        protected void onPostExecute(String output) {
+			outputText.setText(output);
+        }
 	}
- }
+}
